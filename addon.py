6ccf7be5a97cda8926c2e0f6f7cf1407cc0b2525
Jommo Thuy <jthuy@su-f5.msk.21-school.ru>
2020-12-19 16:19:46 +0300

    change func export_mesh2obj

--- addon.py
+++ addon.py
@@ -1,4 +1,5 @@
 import bpy
+import os
 import sys # to work geopandas
 sys.path.append('/Users/jthuy/.local/lib/python3.7/site-packages')
 #import pandas as pd
@@ -8,7 +9,6 @@ import numpy as np
 path = "/Users/jthuy/Desktop/BConv/"
 file = "engenier_network.gpkg"
 
-pipe_diameter = 0.2
 pepe_resolution = 2 # equally 8 edges
 box_size = (1, 1, 1)
 
@@ -16,21 +16,62 @@ box_size = (1, 1, 1)
 def main():
 	clear_scene()
 	data = gpd.read_file(path + file)
-	df = data[['ID', 'SECT_TYPE', 'SECT_WIDTH', 'SECT_HEIGH', 'H1', 'geometry']]
+	
+#	df = data[['ID', 'SECT_TYPE', 'SECT_WIDTH', 'SECT_HEIGH', 'H1', 'geometry']]
+#	for string in range(len(df)):
+#		id = int(df['ID'][string])
+#		vertex = len(df['geometry'][string][0].xy[0])	
+#		for vertex in range(len(df['geometry'][string][0].xy[0]) - 1):
+#			crd_x1 = round(df['geometry'][string][0].xy[0][vertex], 3)
+#			crd_y1 = round(df['geometry'][string][0].xy[1][vertex], 3)
+#			crd_x2 = round(df['geometry'][string][0].xy[0][vertex + 1], 3)
+#			crd_y2 = round(df['geometry'][string][0].xy[1][vertex + 1], 3)
+#			crd_z = -round(df['H1'][string].item(), 3)
+#			# df['H1'][string] = int(df['H1'][string]) #
+#			# print(type(df['H1'][string])) #
+#			create_pipe(crd_x1, crd_y1, crd_x2, crd_y2, crd_z)
+#			break
+#		break
+
+	## FOR TEST
+	df = data[['ID', 'SECT_TYPE', 'SECT_WIDTH', 'SECT_HEIGH', 'X1', 'Y1', 'Z1', 'X2', 'Y2', 'Z2']]
 	for string in range(len(df)):
-		id = int(df['ID'][string])
-		vertex = len(df['geometry'][string][0].xy[0])	
-		for vertex in range(len(df['geometry'][string][0].xy[0]) - 1):
-			crd_x1 = round(df['geometry'][string][0].xy[0][vertex], 3)
-			crd_y1 = round(df['geometry'][string][0].xy[1][vertex], 3)
-			crd_x2 = round(df['geometry'][string][0].xy[0][vertex + 1], 3)
-			crd_y2 = round(df['geometry'][string][0].xy[1][vertex + 1], 3)
-			crd_z = -round(df['H1'][string].item(), 3)
-			# df['H1'][string] = int(df['H1'][string]) #
-			# print(type(df['H1'][string])) #
-			create_pipe(crd_x1, crd_y1, crd_x2, crd_y2, crd_z)
-			break
-		break
+		id = str(int(df['ID'][string].item()))
+		x1 = round(df['X1'][string].item(), 3)
+		y1 = round(df['Y1'][string].item(), 3)
+		z1 = -round(df['Z1'][string].item(), 3)
+		x2 = round(df['X2'][string].item(), 3)
+		y2 = round(df['Y2'][string].item(), 3)
+		z2 = -round(df['Z2'][string].item(), 3)
+		d = round(df['SECT_HEIGH'][string].item() / 1000, 3) # d / 1000 <--- FOR TEST
+		
+		if not string:
+			create_pipeline(id, x1, y1, z1, x2, y2, z2, d)
+			bpy.ops.object.select_all(action='DESELECT')
+			continue
+		
+		for object in bpy.data.objects:
+			if object.name == id:
+				object.select_set(1)
+				add_pipe(x1, y1, z1, x2, y2, z2, d)
+				break
+		else:
+			create_pipeline(id, x1, y1, z1, x2, y2, z2, d)
+		bpy.ops.object.select_all(action='DESELECT')
+		
+	if not os.path.isdir(path + "result"):
+		os.mkdir(path + "result")
+			
+	for object in bpy.data.objects:
+		object.select_set(1)
+		set_pipelinesetting(bpy.context.selected_objects[0])
+		export_mesh2obj()
+		object.select_set(0)
+		
+		
+#		set_pipelinesetting(bpy.context.selected_objects[0])
+#		break
+#	bpy.ops.object.select_all(action='DESELECT')
 
 #	parsing_file() NEED DELß
 #	create_mesh()
@@ -42,27 +83,26 @@ def clear_scene():
 	bpy.ops.object.delete()	
 
 
-def parsing_file():
-	data = gpd.read_file(path + file)
-	print(data.head(1))
-	
+#def parsing_file():
+#	data = gpd.read_file(path + file)
+#	print(data.head(1))	
 
-def create_mesh():
+#def create_mesh():
 #	create_pipeline()
-	create_box((0, 0, 0))
+#	create_box((0, 0, 0))
 	
-def create_pipeline():
-	create_pipe()
-	extrude_pipe((2, 0, 2))
-	add_pipe()
-	set_pipelinesetting(bpy.context.selected_objects[0])
+#def create_pipeline():
+#	create_pipe()
+#	extrude_pipe((2, 0, 2))
+#	add_pipe()
+#	set_pipelinesetting(bpy.context.selected_objects[0])
 	
-def create_pipe(crd_x1, crd_y1, crd_x2, crd_y2, crd_z):
-#	print(type(crd_z), crd_z)
+def create_pipeline(id, x1, y1, z1, x2, y2, z2, d):
 	bpy.ops.curve.primitive_bezier_curve_add()
-	set_vertpos(bpy.context.selected_objects[0].data.splines[0].bezier_points[0], (crd_x1, crd_y1, crd_z))
-	set_vertpos(bpy.context.selected_objects[0].data.splines[0].bezier_points[1], (crd_x2, crd_y2, crd_z))
-	set_pipelinesetting(bpy.context.selected_objects[0]) # TMP LINE
+	bpy.context.selected_objects[0].name = id
+	set_vertpos(bpy.context.selected_objects[0].data.splines[0].bezier_points[0], (x1, y1, z1))
+	set_vertpos(bpy.context.selected_objects[0].data.splines[0].bezier_points[1], (x2, y2, z2))
+	bpy.context.selected_objects[0].data.bevel_depth = d
 	
 def extrude_pipe(pos):
 	bpy.ops.object.editmode_toggle()
@@ -79,15 +119,15 @@ def extrude_pipe(pos):
 	bpy.ops.transform.transform(mode='CURVE_SHRINKFATTEN', value=(1.4, 0, 0, 0))
 	bpy.ops.object.editmode_toggle()
 	
-def add_pipe():
+def add_pipe(x1, y1, z1, x2, y2, z2, d):
 	bpy.ops.object.editmode_toggle()
 	bpy.ops.curve.primitive_bezier_curve_add()
-	set_vertpos(bpy.context.edit_object.data.splines[1].bezier_points[0], (1, 0, 0))
-	set_vertpos(bpy.context.edit_object.data.splines[1].bezier_points[1], (1, 0, 2))
+	set_vertpos(bpy.context.edit_object.data.splines[1].bezier_points[0], (x1, y1, z1))
+	set_vertpos(bpy.context.edit_object.data.splines[1].bezier_points[1], (x2, y2, z2))
+	bpy.context.selected_objects[0].data.bevel_depth = d
 	bpy.ops.object.editmode_toggle()
 	
 def set_pipelinesetting(pipeline):
-	pipeline.data.bevel_depth = pipe_diameter
 	pipeline.data.bevel_resolution = pepe_resolution
 	bpy.ops.object.editmode_toggle()
 	bpy.ops.curve.select_all(action='SELECT')
@@ -117,15 +157,19 @@ def export_meshes():
 		export_mesh2obj()
 		mesh.select_set(0)
 
-def export_mesh2obj(index=[1]):
-	suffix = ""
-	if index[0] < 10:
-		suffix = "00"
-	elif index[0] < 100:
-		suffix = "0"
-	name = f"object_{suffix}{index[0]}.obj"
-	bpy.ops.export_scene.obj(filepath=(path+name), use_selection=True, use_materials=False)
-	index[0] += 1
+#def export_mesh2obj(index=[1]):
+#	suffix = ""
+#	if index[0] < 10:
+#		suffix = "00"
+#	elif index[0] < 100:
+#		suffix = "0"
+#	name = f"object_{suffix}{index[0]}.obj"
+#	bpy.ops.export_scene.obj(filepath=(path+name), use_selection=True, use_materials=False)
+#	index[0] += 1
+
+def export_mesh2obj():
+	name = bpy.context.selected_objects[0].name
+	bpy.ops.export_scene.obj(filepath=(path+"result/"+name), use_selection=True, use_materials=False)
 
 
 if __name__ == "__main__":
--- script.py
+++ script.py
@@ -1,8 +1,14 @@
 # import fiona
-import geopandas
+# import geopandas
 
-countries_gdf = geopandas.read_file('/Users/jthuy/Desktop/BConv/engenier_network.gpkg')
+# countries_gdf = geopandas.read_file('/Users/jthuy/Desktop/BConv/engenier_network.gpkg')
 
-print(countries_gdf.tail(291))
+# print(countries_gdf.tail(291))
 
+import os
 
+path = "/Users/jthuy/Desktop/BConv/"
+file = "engenier_network.gpkg"
+
+if not os.path.isdir(path + "result"):
+	os.mkdir(path + "result")
